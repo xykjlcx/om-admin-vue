3100a934da937313734862a99b60c8c75930b8f0
Ocean.Li <348686686@qq.com>
2018-10-15 16:33:15 +0800

    增加搜索时，回车事件监听

--- src/views/courses/all.vue
+++ src/views/courses/all.vue
@@ -23,7 +23,7 @@
           <!-- 表格操作头部 -->
           <el-row :gutter="20">
             <el-col :span="8">
-              <el-input v-model="searchContent" placeholder="请输入内容"></el-input>
+              <el-input v-model="searchContent" placeholder="请输入内容" @keyup.enter.native="searchCourse"></el-input>
             </el-col>
             <el-col :span="12">
               <el-button type="primary" @click="searchCourse">搜索</el-button>
