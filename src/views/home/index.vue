4cfd5d736186473419337fd8f029ceea8957ca8d
Ocean.Li <348686686@qq.com>
2018-10-25 23:46:02 +0800

    feat：首页整合echarts完成数据分析(模拟)

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -8,7 +8,7 @@
           </div>
           <div class="card-panel-description">
             <div class="card-panel-text">学生总数</div>
-            <span class="home-label-font">1000</span>
+            <span class="home-label-font">8686</span>
           </div>
         </div>
       </el-col>
@@ -19,7 +19,7 @@
           </div>
           <div class="card-panel-description">
             <div class="card-panel-text">最受欢迎的课程</div>
-            <span class="home-label-font">AV</span>
+            <span style="font-size:20px">Spring入门</span>
           </div>
         </div>
       </el-col>
@@ -30,7 +30,7 @@
           </div>
           <div class="card-panel-description">
             <div class="card-panel-text">最新评论</div>
-            <span class="home-label-font">卧槽</span>
+            <span style="font-size:20px">老师很用心</span>
           </div>
         </div>
       </el-col>
@@ -47,14 +47,258 @@
     </el-col> -->
     </el-row>
 
+    <div id="myChart" style="width:100%;height:100vh"></div>
+
   </div>
 </template>
 
 <script>
+  import $ from 'jquery'
+
+
   export default {
+    mounted() {
+      this.drawChart();
+    },
     methods: {
       handleSetLineChartData(type) {
         this.$emit('handleSetLineChartData', type)
+      },
+      drawChart() {
+        // 基于准备好的dom，初始化echarts实例
+        let myChart = this.$echarts.init(document.getElementById('myChart'))
+        var xData = function () {
+          var data = [];
+          for (var i = 1; i < 13; i++) {
+            data.push(i + "月份");
+          }
+          return data;
+        }();
+
+        var option = {
+          backgroundColor: "#ffffff",
+          "title": {
+            "text": "OceanMooc年度注册用户统计",
+            "subtext": "模拟数据展示",
+            x: "4%",
+
+            textStyle: {
+              color: '#32CCBC',
+              fontSize: '22'
+            },
+            subtextStyle: {
+              color: '#90979c',
+              fontSize: '16',
+
+            },
+          },
+          "tooltip": {
+            "trigger": "axis",
+            "axisPointer": {
+              "type": "shadow",
+              textStyle: {
+                color: "#fff"
+              }
+
+            },
+          },
+          "grid": {
+            "borderWidth": 0,
+            "top": 110,
+            "bottom": 95,
+            textStyle: {
+              color: "#fff"
+            }
+          },
+          "legend": {
+            x: '4%',
+            top: '11%',
+            textStyle: {
+              color: '#90979c',
+            },
+            "data": ['女', '男', '平均']
+          },
+
+
+          "calculable": true,
+          "xAxis": [{
+            "type": "category",
+            "axisLine": {
+              lineStyle: {
+                color: '#90979c'
+              }
+            },
+            "splitLine": {
+              "show": false
+            },
+            "axisTick": {
+              "show": false
+            },
+            "splitArea": {
+              "show": false
+            },
+            "axisLabel": {
+              "interval": 0,
+
+            },
+            "data": xData,
+          }],
+          "yAxis": [{
+            "type": "value",
+            "splitLine": {
+              "show": false
+            },
+            "axisLine": {
+              lineStyle: {
+                color: '#90979c'
+              }
+            },
+            "axisTick": {
+              "show": false
+            },
+            "axisLabel": {
+              "interval": 0,
+
+            },
+            "splitArea": {
+              "show": false
+            },
+
+          }],
+          "dataZoom": [{
+            "show": true,
+            "height": 30,
+            "xAxisIndex": [
+              0
+            ],
+            bottom: 30,
+            "start": 10,
+            "end": 80,
+            handleIcon: 'path://M306.1,413c0,2.2-1.8,4-4,4h-59.8c-2.2,0-4-1.8-4-4V200.8c0-2.2,1.8-4,4-4h59.8c2.2,0,4,1.8,4,4V413z',
+            handleSize: '110%',
+            handleStyle: {
+              color: "#d3dee5",
+
+            },
+            textStyle: {
+              color: "#fff"
+            },
+            borderColor: "#90979c"
+
+
+          }, {
+            "type": "inside",
+            "show": true,
+            "height": 15,
+            "start": 1,
+            "end": 35
+          }],
+          "series": [{
+              "name": "女",
+              "type": "bar",
+              "stack": "总量",
+              "barMaxWidth": 35,
+              "barGap": "10%",
+              "itemStyle": {
+                "normal": {
+                  "color": "rgba(255,144,128,1)",
+                  "label": {
+                    "show": true,
+                    "textStyle": {
+                      "color": "#fff"
+                    },
+                    "position": "insideTop",
+                    formatter: function (p) {
+                      return p.value > 0 ? (p.value) : '';
+                    }
+                  }
+                }
+              },
+              "data": [
+                709,
+                1917,
+                2455,
+                2610,
+                1719,
+                1433,
+                1544,
+                3285,
+                5208,
+                3372,
+                2484,
+                4078
+              ],
+            },
+
+            {
+              "name": "男",
+              "type": "bar",
+              "stack": "总量",
+              "itemStyle": {
+                "normal": {
+                  "color": "rgba(0,191,183,1)",
+                  "barBorderRadius": 0,
+                  "label": {
+                    "show": true,
+                    "position": "top",
+                    formatter: function (p) {
+                      return p.value > 0 ? (p.value) : '';
+                    }
+                  }
+                }
+              },
+              "data": [
+                327,
+                1776,
+                507,
+                1200,
+                800,
+                482,
+                204,
+                1390,
+                1001,
+                951,
+                381,
+                220
+              ]
+            }, {
+              "name": "总数",
+              "type": "line",
+              "stack": "总量",
+              symbolSize: 10,
+              symbol: 'circle',
+              "itemStyle": {
+                "normal": {
+                  "color": "#0396FF",
+                  "barBorderRadius": 0,
+                  "label": {
+                    "show": true,
+                    "position": "top",
+                    formatter: function (p) {
+                      return p.value > 0 ? (p.value) : '';
+                    }
+                  }
+                }
+              },
+              "data": [
+                1036,
+                3693,
+                2962,
+                3810,
+                2519,
+                1915,
+                1748,
+                4675,
+                6209,
+                4323,
+                2865,
+                4298
+              ]
+            },
+          ]
+        };
+        // 绘制图表
+        myChart.setOption(option);
       }
     }
   }
